b2f596e3474be97933729e75a36229379a0af519
thiagoweverson <124640710+thiagoweverson@users.noreply.github.com>
2023-06-18 12:52:54 -0300

    Add files via upload

---
+++ beta/quartos/index.js
@@ -0,0 +1,243 @@
+function inicio() {
+    document.getElementById("quarto_01").style.display = 'none';
+    document.getElementById("quarto_02").style.display = 'none';
+    document.getElementById("quarto_03").style.display = 'none';
+    document.getElementById("quarto_04").style.display = 'none';
+    document.getElementById("quarto_05").style.display = 'none';
+    document.getElementById("quarto_06").style.display = 'none';
+    document.getElementById("quarto_07").style.display = 'none';
+    document.getElementById("quarto_08").style.display = 'none';
+    document.getElementById("quarto_09").style.display = 'none';
+    document.getElementById("quarto_10").style.display = 'none';
+    document.getElementById("quarto_11").style.display = 'none';
+    document.getElementById("quarto_12").style.display = 'none';
+}
+
+function q_01() {
+    document.getElementById("quarto_01").style.display = 'block';
+    document.getElementById("quarto_02").style.display = 'none';
+    document.getElementById("quarto_03").style.display = 'none';
+    document.getElementById("quarto_04").style.display = 'none';
+    document.getElementById("quarto_05").style.display = 'none';
+    document.getElementById("quarto_06").style.display = 'none';
+    document.getElementById("quarto_07").style.display = 'none';
+    document.getElementById("quarto_08").style.display = 'none';
+    document.getElementById("quarto_09").style.display = 'none';
+    document.getElementById("quarto_10").style.display = 'none';
+    document.getElementById("quarto_11").style.display = 'none';
+    document.getElementById("quarto_12").style.display = 'none';
+}
+
+function q_02() {
+    document.getElementById("quarto_01").style.display = 'none';
+    document.getElementById("quarto_02").style.display = 'block';
+    document.getElementById("quarto_03").style.display = 'none';
+    document.getElementById("quarto_04").style.display = 'none';
+    document.getElementById("quarto_05").style.display = 'none';
+    document.getElementById("quarto_06").style.display = 'none';
+    document.getElementById("quarto_07").style.display = 'none';
+    document.getElementById("quarto_08").style.display = 'none';
+    document.getElementById("quarto_09").style.display = 'none';
+    document.getElementById("quarto_10").style.display = 'none';
+    document.getElementById("quarto_11").style.display = 'none';
+    document.getElementById("quarto_12").style.display = 'none';
+}
+
+
+function q_03() {
+    document.getElementById("quarto_01").style.display = 'none';
+    document.getElementById("quarto_02").style.display = 'none';
+    document.getElementById("quarto_03").style.display = 'block';
+    document.getElementById("quarto_04").style.display = 'none';
+    document.getElementById("quarto_05").style.display = 'none';
+    document.getElementById("quarto_06").style.display = 'none';
+    document.getElementById("quarto_07").style.display = 'none';
+    document.getElementById("quarto_08").style.display = 'none';
+    document.getElementById("quarto_09").style.display = 'none';
+    document.getElementById("quarto_10").style.display = 'none';
+    document.getElementById("quarto_11").style.display = 'none';
+    document.getElementById("quarto_12").style.display = 'none';
+}
+
+
+function q_04() {
+    document.getElementById("quarto_01").style.display = 'none';
+    document.getElementById("quarto_02").style.display = 'none';
+    document.getElementById("quarto_03").style.display = 'none';
+    document.getElementById("quarto_04").style.display = 'block';
+    document.getElementById("quarto_05").style.display = 'none';
+    document.getElementById("quarto_06").style.display = 'none';
+    document.getElementById("quarto_07").style.display = 'none';
+    document.getElementById("quarto_08").style.display = 'none';
+    document.getElementById("quarto_09").style.display = 'none';
+    document.getElementById("quarto_10").style.display = 'none';
+    document.getElementById("quarto_11").style.display = 'none';
+    document.getElementById("quarto_12").style.display = 'none';
+}
+
+function q_05() {
+    document.getElementById("quarto_01").style.display = 'none';
+    document.getElementById("quarto_02").style.display = 'none';
+    document.getElementById("quarto_03").style.display = 'none';
+    document.getElementById("quarto_04").style.display = 'none';
+    document.getElementById("quarto_05").style.display = 'block';
+    document.getElementById("quarto_06").style.display = 'none';
+    document.getElementById("quarto_07").style.display = 'none';
+    document.getElementById("quarto_08").style.display = 'none';
+    document.getElementById("quarto_09").style.display = 'none';
+    document.getElementById("quarto_10").style.display = 'none';
+    document.getElementById("quarto_11").style.display = 'none';
+    document.getElementById("quarto_12").style.display = 'none';
+}
+
+function q_06() {
+    document.getElementById("quarto_01").style.display = 'none';
+    document.getElementById("quarto_02").style.display = 'none';
+    document.getElementById("quarto_03").style.display = 'none';
+    document.getElementById("quarto_04").style.display = 'none';
+    document.getElementById("quarto_05").style.display = 'none';
+    document.getElementById("quarto_06").style.display = 'block';
+    document.getElementById("quarto_07").style.display = 'none';
+    document.getElementById("quarto_08").style.display = 'none';
+    document.getElementById("quarto_09").style.display = 'none';
+    document.getElementById("quarto_10").style.display = 'none';
+    document.getElementById("quarto_11").style.display = 'none';
+    document.getElementById("quarto_12").style.display = 'none';
+}
+
+function q_07() {
+    document.getElementById("quarto_01").style.display = 'none';
+    document.getElementById("quarto_02").style.display = 'none';
+    document.getElementById("quarto_03").style.display = 'none';
+    document.getElementById("quarto_04").style.display = 'none';
+    document.getElementById("quarto_05").style.display = 'none';
+    document.getElementById("quarto_06").style.display = 'none';
+    document.getElementById("quarto_07").style.display = 'block';
+    document.getElementById("quarto_08").style.display = 'none';
+    document.getElementById("quarto_09").style.display = 'none';
+    document.getElementById("quarto_10").style.display = 'none';
+    document.getElementById("quarto_11").style.display = 'none';
+    document.getElementById("quarto_12").style.display = 'none';
+}
+
+function q_08() {
+    document.getElementById("quarto_01").style.display = 'none';
+    document.getElementById("quarto_02").style.display = 'none';
+    document.getElementById("quarto_03").style.display = 'none';
+    document.getElementById("quarto_04").style.display = 'none';
+    document.getElementById("quarto_05").style.display = 'none';
+    document.getElementById("quarto_06").style.display = 'none';
+    document.getElementById("quarto_07").style.display = 'none';
+    document.getElementById("quarto_08").style.display = 'block';
+    document.getElementById("quarto_09").style.display = 'none';
+    document.getElementById("quarto_10").style.display = 'none';
+    document.getElementById("quarto_11").style.display = 'none';
+    document.getElementById("quarto_12").style.display = 'none';
+}
+
+function q_09() {
+    document.getElementById("quarto_01").style.display = 'none';
+    document.getElementById("quarto_02").style.display = 'none';
+    document.getElementById("quarto_03").style.display = 'none';
+    document.getElementById("quarto_04").style.display = 'none';
+    document.getElementById("quarto_05").style.display = 'none';
+    document.getElementById("quarto_06").style.display = 'none';
+    document.getElementById("quarto_07").style.display = 'none';
+    document.getElementById("quarto_08").style.display = 'none';
+    document.getElementById("quarto_09").style.display = 'block';
+    document.getElementById("quarto_10").style.display = 'none';
+    document.getElementById("quarto_11").style.display = 'none';
+    document.getElementById("quarto_12").style.display = 'none';
+}
+
+function q_10() {
+    document.getElementById("quarto_01").style.display = 'none';
+    document.getElementById("quarto_02").style.display = 'none';
+    document.getElementById("quarto_03").style.display = 'none';
+    document.getElementById("quarto_04").style.display = 'none';
+    document.getElementById("quarto_05").style.display = 'none';
+    document.getElementById("quarto_06").style.display = 'none';
+    document.getElementById("quarto_07").style.display = 'none';
+    document.getElementById("quarto_08").style.display = 'none';
+    document.getElementById("quarto_09").style.display = 'none';
+    document.getElementById("quarto_10").style.display = 'block';
+    document.getElementById("quarto_11").style.display = 'none';
+    document.getElementById("quarto_12").style.display = 'none';
+}
+
+function q_11() {
+    document.getElementById("quarto_01").style.display = 'none';
+    document.getElementById("quarto_02").style.display = 'none';
+    document.getElementById("quarto_03").style.display = 'none';
+    document.getElementById("quarto_04").style.display = 'none';
+    document.getElementById("quarto_05").style.display = 'none';
+    document.getElementById("quarto_06").style.display = 'none';
+    document.getElementById("quarto_07").style.display = 'none';
+    document.getElementById("quarto_08").style.display = 'none';
+    document.getElementById("quarto_09").style.display = 'none';
+    document.getElementById("quarto_10").style.display = 'none';
+    document.getElementById("quarto_11").style.display = 'block';
+    document.getElementById("quarto_12").style.display = 'none';
+}
+
+function q_12() {
+    document.getElementById("quarto_01").style.display = 'none';
+    document.getElementById("quarto_02").style.display = 'none';
+    document.getElementById("quarto_03").style.display = 'none';
+    document.getElementById("quarto_04").style.display = 'none';
+    document.getElementById("quarto_05").style.display = 'none';
+    document.getElementById("quarto_06").style.display = 'none';
+    document.getElementById("quarto_07").style.display = 'none';
+    document.getElementById("quarto_08").style.display = 'none';
+    document.getElementById("quarto_09").style.display = 'none';
+    document.getElementById("quarto_10").style.display = 'none';
+    document.getElementById("quarto_11").style.display = 'none';
+    document.getElementById("quarto_12").style.display = 'block';
+}
+
+
+
+
+
+
+function quar() {
+    var display = document.getElementById(el).style.display;
+    if(display == "none"){
+        document.getElementById(quarto_01).style.display = 'block';        
+    }                    
+    else{
+        document.getElementById(quarto_02).style.display = 'none';
+    }
+}
+
+
+        
+function toggle2(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "none"){
+        document.getElementById(el).style.display = 'block';        
+    }                    
+    else{
+        document.getElementById(el).style.display = 'none';
+        }
+}
+
+
+
+
+
+function esconde2(el) {
+    document.getElementById(el).style.display = 'none';
+}
+
+function quarto_01() {
+    document.getElementById(qo_01).style.display = 'block';
+        document.getElementById(q_02).style.display = 'none';
+
+}
+
+function quarto_02() {
+    document.getElementById(q_02).style.display = 'block';
+    document.getElementById(q_01).style.display = 'none';
+
+}
